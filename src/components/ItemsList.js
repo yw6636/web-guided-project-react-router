59d9a381e2a35b2e59e5d41c06280689454cb5c0
yw6636 <yw6636@gmail.com>
2022-11-19 18:04:07 -0500

    answered all of the tasks

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,8 +1,9 @@
 import React from 'react'
 // We'll need a Link and the useRouteMatch hook from 'react-router-dom'
-
+import { Link, useRouteMatch } from 'react-router-dom'
 export default function ItemsList(props) {
   const { items } = props
+  const { url } = useRouteMatch();
 
   // We'll grab the current URL using the hook
 
@@ -14,14 +15,16 @@ export default function ItemsList(props) {
           key={item.id}
         >
           {/* 👉 STEP 6 - Link starts, navigates us from <current url> to <current url>/<id of the item> */}
-          <img
-            className='items-list-image'
-            src={item.imageUrl}
-            alt={item.name}
-          />
-          <p>{item.name}</p>
+          <Link to={`${url}/${item.id}`}>
+          {/* We could do <Link to={'localhost:1234/items-list/${item.id}}/>  */}
+            <img
+              className='items-list-image'
+              src={item.imageUrl}
+              alt={item.name}
+            />
+            <p>{item.name}</p>
           {/* Link ends */}
-
+          </Link>
           <p>${item.price}</p>
         </div>
       ))}
--- src/index.js
+++ src/index.js
@@ -8,9 +8,17 @@ import App from './components/App'
 import './styles.less'
 
 // 👉 STEP 1 - Import React Router's Router
-
+import { BrowserRouter as Router } from 'react-router-dom';
 render(
   // Wrap the <App /> in a provider
-  <App />
+  <Router>
+    <App />
+  </Router>
   , document.querySelector('#root')
 )
+
+/** 
+ * location -> All data about where you are
+ * history -> All data about where you were and where you're going
+ * match -> All data about the current location with regards to query params etc 
+ */
